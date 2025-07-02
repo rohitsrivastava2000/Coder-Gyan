32d565cec8263e2179e0153e6d62da58faa6d815
rohitsrivastava2000 <bhanusrivastavadlw@gmail.com>
2025-07-02 12:51:45 +0530

    adding images in assets folder

--- Backend/Languages/cpp-runner.cpp
+++ Backend/Languages/cpp-runner.cpp
@@ -1,34 +1,9 @@
-#include <iostream>
-#include <vector>
+// Welcome To Coder'$ Gyan
+#include<iostream>
+
 using namespace std;
 
-bool isPrimeUsingSieve(int n) {
-    if (n < 2) return false;
-
-    // Create a boolean array "isPrime[0..n]" and initialize all entries as true
-    vector<bool> isPrime(n + 1, true);
-    isPrime[0] = isPrime[1] = false;
-
-    // Sieve of Eratosthenes
-    for (int i = 2; i * i <= n; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= n; j += i)
-                isPrime[j] = false;
-        }
-    }
-
-    return isPrime[n];
-}
-
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-
-    if (isPrimeUsingSieve(num))
-        cout << num << " is a prime number.\n";
-    else
-        cout << num << " is not a prime number.\n";
-
+int main(){
+    cout<<"Hello World!";
     return 0;
 }
